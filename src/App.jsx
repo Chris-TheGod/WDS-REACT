fd8ea588130210fd124dd43e38c5633c78f4e735
Kristjan Lääne <xonproject@gmail.com>
2023-08-01 13:44:33 +0300

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import React from 'react'
+import './styles.css'
+import user from './user.json'
 
 function App() {
-  return (
-    <>
-      <div className='large' id='largeDiv'>
-        <label htmlFor='inputId'>Ooga</label>
-        <input type='number' value={3} id='inputId' />
-      </div>
-    </>
-  )
+  return <div>{JSON.stringify(user)}</div>
 }
 
 export default App
