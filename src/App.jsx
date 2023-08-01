145e66b9f1a8c6bd3e76735463b30c405ae60d34
Kristjan Lääne <xonproject@gmail.com>
2023-08-01 09:52:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='large' id='largeDiv'>
+        <label htmlFor='inputId'>Ooga</label>
+        <input type='number' value={3} id='inputId' />
+      </div>
+    </>
+  )
+}
+
+export default App
