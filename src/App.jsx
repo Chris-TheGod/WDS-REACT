2063cc3fe7af0f89f48b91308de408ef024fd971
Kristjan Lääne <xonproject@gmail.com>
2023-08-03 08:44:15 +0300

    counter project

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,24 @@
 import { useState } from 'react'
 
-const INITIAL_VALUE = ['A', 'B', 'C']
-
 function App() {
-  const [array, setArray] = useState(INITIAL_VALUE)
-  const [value, setValue] = useState('')
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
 
-  function removeFirstElement() {
-    setArray((currentArray) => {
-      return currentArray.slice(1)
-    })
-  }
-
-  function removeSpecificLetter(letter) {
-    setArray((currentArray) => {
-      return currentArray.filter((element) => element !== letter)
-    })
-  }
-
-  function addLetterToStart(letter) {
-    setArray((currentArray) => {
-      return [letter, ...currentArray]
-    })
-  }
-
-  function addLetterToEnd(letter) {
-    setArray((currentArray) => {
-      return [...currentArray, letter]
-    })
-  }
-
-  function clear() {
-    setArray([])
-  }
-
-  function reset() {
-    setArray(INITIAL_VALUE)
-  }
-
-  function updateAToH() {
-    setArray((currentArray) => {
-      return currentArray.map((element) => {
-        if (element === 'A') return 'H'
-        return element
-      })
-    })
-  }
-
-  function addLetterAtIndex(letter, index) {
-    setArray((currentArray) => {
-      return [
-        ...currentArray.slice(0, index),
-        letter,
-        ...currentArray.slice(index),
-      ]
-    })
-  }
+  const handleMinusClick = () => setAge(age - 1)
+  const handlePlusClick = () => setAge(age + 1)
 
   return (
-    <div>
-      <button onClick={removeFirstElement}>Remove first element</button>
+    <>
+      <input onChange={(e) => setName(e.target.value)} />
       <br />
-      <button onClick={() => removeSpecificLetter('B')}>Remove all B`s</button>
+      <button onClick={handleMinusClick}>-</button>
+      The age is: {age}
+      <button onClick={handlePlusClick}>+</button>
       <br />
-      <button onClick={() => addLetterToStart('B')}>Add to start</button>
-      <br />
-      <button onClick={() => addLetterToEnd('Z')}>Add to end</button>
-      <br />
-      <button onClick={clear}>Clear</button>
-      <br />
-      <button onClick={reset}>Reset</button>
-      <br />
-      <button onClick={updateAToH}>Uptade A to H</button>
-      <br />
-      <button onClick={() => addLetterAtIndex('C', 2)}>Add C at 2</button>
-      <br />
-      <input onChange={(e) => setValue(e.target.value)} value={value} />
-      <br />
-      <button onClick={() => addLetterToStart(value)}>
-        Add Value To Array
-      </button>
-
-      <br />
-      {array.join(', ')}
-    </div>
+      <div>
+        The name is: {name} and the age is: {age}
+      </div>
+    </>
   )
 }
 
