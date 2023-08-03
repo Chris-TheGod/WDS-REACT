b3d0aa741d55a8bfd61b93f2774ff9b8d8c6f9ac
Kristjan Lääne <xonproject@gmail.com>
2023-08-03 07:58:42 +0300

    array task

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,60 @@
-import './styles.css'
-import user from './user.json'
+import { useState } from 'react'
+
+const INITIAL_VALUE = ['A', 'B', 'C']
 
 function App() {
-  return <div>{JSON.stringify(user)}</div>
+  const [array, setArray] = useState(INITIAL_VALUE)
+
+  function removeFirstElement() {
+    setArray((currentArray) => {
+      return currentArray.slice(1)
+    })
+  }
+
+  function removeSpecificLetter(letter) {
+    setArray((currentArray) => {
+      return currentArray.filter((element) => element !== letter)
+    })
+  }
+
+  function addLetterToStart(letter) {
+    setArray((currentArray) => {
+      return [letter, ...currentArray]
+    })
+  }
+
+  function addLetterToEnd(letter) {
+    setArray((currentArray) => {
+      return [...currentArray, letter]
+    })
+  }
+
+  function clear() {
+    setArray([])
+  }
+
+  function reset() {
+    setArray(INITIAL_VALUE)
+  }
+
+  return (
+    <div>
+      <button onClick={removeFirstElement}>Remove first element</button>
+      <br />
+      <button onClick={() => removeSpecificLetter('B')}>Remove all B`s</button>
+      <br />
+      <button onClick={() => addLetterToStart('B')}>Add to start</button>
+      <br />
+      <button onClick={() => addLetterToEnd('Z')}>Add to end</button>
+      <br />
+      <button onClick={clear}>Clear</button>
+      <br />
+      <button onClick={reset}>Reset</button>
+      <br />
+      <br />
+      {array.join(', ')}
+    </div>
+  )
 }
 
 export default App
